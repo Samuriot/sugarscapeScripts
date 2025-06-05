e2cf8e81920259a4db4f5ac04cd40d3c3aeb7808
Bryan Kim <wabkim360@gmail.com>
2025-06-04 23:52:24 -0700

    added save_batch.py

---
+++ save_batch.py
@@ -0,0 +1,54 @@
+import sys
+import glob
+import json
+
+def open_file (filename):
+    with open(filename, 'r') as file:
+        return json.load(file)
+
+def accumulate (file_list):
+    total_map = {}
+    for filename in file_list:
+        entries = open_file(filename)
+
+        for entry in entries:
+            t = entry["timestep"]
+            if t not in total_map:
+                total_map[t] = {"count": 0}
+            total_map[t]["count"] += 1
+            
+            for k, val in entry.items():
+                if k == "timestep":
+                    continue
+                sum_key = f"{k}_sum"
+                total_map[t].setdefault(sum_key, 0)
+                total_map[t][sum_key] += val
+            
+    return total_map
+
+def average (total_results, allowed_keys):
+    average_results = []
+    for t in sorted(total_results.keys()):
+        info = {"timestep": t}
+        count = total_results[t]["count"]
+        for sum_key, total in total_results[t].items():
+            if sum_key == "count":
+                continue
+            field = sum_key.replace("_sum", "")
+            if field in allowed_keys:
+                info[field] = total / count
+            
+        average_results.append(info)
+    
+    return average_results
+
+if __name__ == "__main__":
+    allowed_keys = {"timestep", "population", "agentDiseaseDeaths", "sickAgentsPercentage"}
+    subdirectory, startingDiseases = sys.argv[1], sys.argv[2]
+    output_file = f"./{subdirectory}/average_log_{startingDiseases}.json"
+
+    file_list = sorted(glob.glob(f"./{subdirectory}/log_{startingDiseases}_*.json"))
+    total_results = accumulate(file_list)
+    average_results = average(total_results, allowed_keys)
+    with open(output_file, "w") as outfile:
+        json.dump(average_results, outfile, indent=2)
